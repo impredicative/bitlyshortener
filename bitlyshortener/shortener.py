32e77476cccbba0028997619df874a8b81738542
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-02 18:05:37 -0500

    Fix type annotation

--- bitlyshortener/shortener.py
+++ bitlyshortener/shortener.py
@@ -1,8 +1,8 @@
-from typing import List
+from typing import List, Optional
 
 
 class Shortener:
-    def __init__(self, *, max_cache_len=512, oauth_tokens: List[str]):
+    def __init__(self, *, max_cache_len: Optional[int] = 512, oauth_tokens: List[str]):
         self._max_cache_len = max_cache_len
         self._oauth_tokens = oauth_tokens
 
