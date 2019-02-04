a41b437b6e18ac81a6354222deb4183db35a07d5
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-03 21:20:31 -0500

    Increase default max cache len

--- bitlyshortener/shortener.py
+++ bitlyshortener/shortener.py
@@ -2,9 +2,9 @@ from typing import List, Optional
 
 
 class Shortener:
-    def __init__(self, *, max_cache_len: Optional[int] = 512, oauth_tokens: List[str]):
+    def __init__(self, *, max_cache_len: Optional[int] = 1024, tokens: List[str]):
         self._max_cache_len = max_cache_len
-        self._oauth_tokens = oauth_tokens
+        self._tokens = tokens
 
     def shorten(self, urls: List[str]) -> List[str]:
         return urls
