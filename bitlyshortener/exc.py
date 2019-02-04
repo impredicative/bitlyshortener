50a5b1b08f8f2370a5d5de62c5b24694c7879d5a
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-04 13:24:59 -0500

    Add logged exceptions

---
+++ bitlyshortener/exc.py
@@ -0,0 +1,22 @@
+"""Exceptions."""
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class ShortenerError(Exception):
+    """This is the base exception class in this module.
+
+    This exception is not raised directly. All other exception classes in this module hierarchically derive from it.
+
+    :param msg: exception error message.
+    """
+    #
+    def __init__(self, msg: str):
+        log.error(msg)
+        super().__init__(msg)
+
+
+class ArgsError(ShortenerError):
+    pass
