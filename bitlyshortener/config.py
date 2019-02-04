dd9a9381b2dee6d36984a5143fcd82c3558f967c
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-04 13:25:30 -0500

    Add logging conf

--- bitlyshortener/config.py
+++ bitlyshortener/config.py
@@ -1,7 +1,45 @@
+import logging.config
+from pathlib import Path
+
+
+def configure_logging() -> None:
+    logging.config.dictConfig(LOGGING)
+    log = logging.getLogger(__name__)
+    log.debug('Logging is configured.')
+
+
 API_URL_BITLINKS = 'https://api-ssl.bitly.com/v4/bitlinks'  # Ref: https://dev.bitly.com/v4/#operation/createFullBitlink
 API_URL_SHORTEN = 'https://api-ssl.bitly.com/v4/shorten'  # Ref: https://dev.bitly.com/v4/#operation/createBitlink
 MAX_CONCURRENT_REQUESTS = 5  # Ref: https://dev.bitly.com/v4/#section/Rate-Limiting
+MIN_CACHE_SIZE = 1024
+PACKAGE_NAME = Path(__file__).parent.stem
 RATE_LIMIT_PER_MINUTE = 100  # Ref: https://dev.bitly.com/v4/#section/Rate-Limiting (for status 201 or 200)
 RATE_LIMIT_PER_HOUR = 1000  # Ref: https://dev.bitly.com/v4/#section/Rate-Limiting (for status 201 or 200)
 RATE_LIMIT_PER_MONTH = 10000  # Ref: https://app.bitly.com/organization/1/detail (requires login) (for status 201)
-REQUEST_TIMEOUT = 3
+REQUEST_TIMEOUT = 2
+TEST_LONG_URL = 'https://python.org/'
+TEST_SHORT_URL = 'https://j.mp/2TtaqZC'
+
+LOGGING = {  # Ref: https://docs.python.org/3/howto/logging.html#configuring-logging
+    'version': 1,
+    'formatters': {
+        'detailed': {
+            'format': '[%(relativeCreated)i] %(name)s:%(lineno)d:%(funcName)s:%(levelname)s: %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'detailed',
+            'stream': 'ext://sys.stdout',
+        },
+    },
+    'loggers': {
+        PACKAGE_NAME: {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
