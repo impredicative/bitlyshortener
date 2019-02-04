d68c1d52da354cfbc6b24b8eb279316289864a5d
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-03 22:52:06 -0500

    Add presumed link of rate limits to status codes

--- bitlyshortener/config.py
+++ bitlyshortener/config.py
@@ -1,7 +1,7 @@
 API_URL_BITLINKS = 'https://api-ssl.bitly.com/v4/bitlinks'  # Ref: https://dev.bitly.com/v4/#operation/createFullBitlink
 API_URL_SHORTEN = 'https://api-ssl.bitly.com/v4/shorten'  # Ref: https://dev.bitly.com/v4/#operation/createBitlink
 MAX_CONCURRENT_REQUESTS = 5  # Ref: https://dev.bitly.com/v4/#section/Rate-Limiting
-RATE_LIMIT_PER_MINUTE = 100  # Ref: https://dev.bitly.com/v4/#section/Rate-Limiting
-RATE_LIMIT_PER_HOUR = 1000  # Ref: https://dev.bitly.com/v4/#section/Rate-Limiting
-RATE_LIMIT_PER_MONTH = 10000  # Ref: https://app.bitly.com/organization/1/detail (requires login)
+RATE_LIMIT_PER_MINUTE = 100  # Ref: https://dev.bitly.com/v4/#section/Rate-Limiting (for status 201 or 200)
+RATE_LIMIT_PER_HOUR = 1000  # Ref: https://dev.bitly.com/v4/#section/Rate-Limiting (for status 201 or 200)
+RATE_LIMIT_PER_MONTH = 10000  # Ref: https://app.bitly.com/organization/1/detail (requires login) (for status 201)
 REQUEST_TIMEOUT = 3
