d7c1419b4702c250a684d688a77c0cd33539e286
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-05 11:37:22 -0500

    Simplify

--- scripts/test_shortener.py
+++ scripts/test_shortener.py
@@ -1,18 +1,36 @@
 import os
+import random
 import time
 
 from bitlyshortener import Shortener
 
 tokens = os.getenv('BITLY_TOKENS').strip().split(',')
 
+URLs = [
+    'https://arxiv.org/abs/1902.01128v1',
+    'https://arxiv.org/abs/1902.01119v1',
+    'https://arxiv.org/abs/1902.01080v1',
+    'https://arxiv.org/abs/1902.01073v1',
+    'https://arxiv.org/abs/1902.01030v1',
+    'https://arxiv.org/abs/1902.00916v1',
+    'https://arxiv.org/abs/1902.00908v1',
+    'https://arxiv.org/abs/1902.00771v1',
+    'https://arxiv.org/abs/1902.00719v1',
+    'https://arxiv.org/abs/1902.00685v1',
+    'https://arxiv.org/abs/1902.00672v1',
+    'https://arxiv.org/abs/1902.00659v1',
+    'https://arxiv.org/abs/1902.00655v1',
+    'https://arxiv.org/abs/1902.00626v1',
+    'https://arxiv.org/abs/1902.00624v1',
+    'https://arxiv.org/abs/1902.00604v1',
+    'https://arxiv.org/abs/1902.00577v1',
+    'https://arxiv.org/abs/1902.00541v1',
+]
+
 try:
     shortener = Shortener(tokens=tokens)
+    urls = random.sample(URLs, k={'none': 0, 'one': 1, 'some': 3, 'all': len(URLs)}['none'])
 
-    # print(shortener.shorten_urls(['https://cnn.com/']))
-    # print(shortener.shorten_urls(['https://cnn.com/']))
-
-    urls = ['https://paperswithcode.com/latest', 'https://towardsdatascience.com/machine-learning/home',
-            'https://research.fb.com/publications/']
     print(shortener.shorten_urls(urls))
     print(shortener.shorten_urls(urls[::-1]))
 
