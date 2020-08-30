6ef6c2dc538a36f45ebbab5204311cef1fa37263
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-08-29 21:09:33 -0400

    Add "--use-feature=2020-resolver" to script which installs requirements

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}"/..
 
 cd ./requirements
-pip install -U -r ./install.txt -U -r ./dev.in
+pip install --use-feature=2020-resolver -U -r ./install.txt -U -r ./dev.in
