4b4a37245638fbe44f75df5614db23eec4fd40ae
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-10-05 14:49:08 -0400

    Update install_requirements.sh

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -3,5 +3,6 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}"/..
 
+pip install -U pip wheel
 cd ./requirements
-pip install --use-feature=2020-resolver -U -r ./install.txt -U -r ./dev.in
+pip install --use-feature=2020-resolver -U -r ./install.in -U -r ./dev.in
